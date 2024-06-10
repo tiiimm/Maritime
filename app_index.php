a7e57438f443c111aa3c20a1acd3c584eb08f9d3
Rheymann14 <67354994+Rheymann14@users.noreply.github.com>
2024-06-10 12:22:05 +0800

    app_index
    landing page and assets

---
+++ app_index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>OBTIMCS</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/finallogo.png" rel="icon">
+  <link href="assets/img/finallogo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/landing_assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/landing_assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/landing_assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/landing_assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/landing_assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/landing_assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/landing_assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/landing_assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1><a href="index.html"><span> OBTIMCS</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#features">Features</a></li>
+ 
+          <li><a class="nav-link scrollto" href="login">Login</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center">
+          <div data-aos="zoom-out">
+            
+            <h1>Welcome to your <span>Maritime Companion!</span></h1>
+            <h2>Commission on Higher Education On Board Training App.</h2>
+            <div class="text-center text-lg-start">
+              <a href="#!" class="btn-get-started scrollto">Download Now!</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="300">
+          <img src="assets/landing_assets/img/hero-img.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+    <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+      <defs>
+        <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+      </defs>
+      <g class="wave1">
+        <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+      </g>
+      <g class="wave2">
+        <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+      </g>
+      <g class="wave3">
+        <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+      </g>
+    </svg>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container-fluid">
+
+        <div class="row">
+          <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch" data-aos="fade-right">
+            <a href="assets/landing_assets/img/presentation.mp4" class="glightbox play-btn mb-4"></a>
+          </div>
+
+          <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" data-aos="fade-left">
+            <h3>VesselVoyage: Real-Time GPS Tracker</h3>
+            <p>Stay connected to your fleet with VesselVoyage, the ultimate app for tracking the live location of your vessels using GPS. Monitor your maritime assets in real-time, ensure safety, and optimize routes with ease.</p>
+
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+              <div class="icon"><i class='bx bx-current-location'></i></div>
+              <h4 class="title"><a href="">Live Location Tracking</a></h4>
+              <p class="description">Track the exact position of your vessels in real-time on an interactive map. Get instant updates on location changes, speed, and direction to ensure your fleet is always on course.</p>
+            </div>
+
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
+              <div class="icon"><i class='bx bxs-map-pin' ></i></div>
+              <h4 class="title"><a href="">Route Optimization</a></h4>
+              <p class="description">Optimize your vessel's routes with advanced algorithms that consider weather conditions, sea traffic, and fuel efficiency. Save time and reduce operational costs with smarter navigation.</p>
+            </div>
+
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="300">
+              <div class="icon"><i class='bx bx-bell-plus' ></i></div>
+              <h4 class="title"><a href="">Safety Alerts and Notifications</a></h4>
+              <p class="description">Receive instant alerts for any deviations, emergencies, or potential hazards. Stay informed with push notifications for events like route changes, geofence breaches, and unexpected stops.</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Features Section ======= -->
+    <section id="features" class="features">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Features</h2>
+          <p>Check The Features</p>
+        </div>
+
+        <div class="row" data-aos="fade-left">
+          <div class="col-lg-3 col-md-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="50">
+            <i class="ri-map-pin-user-fill" style="color: #ffbb2c;"></i>
+              <h3><a href="">Live Location Tracking</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+              <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+              <h3><a href="">Real Time Statistics</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="150">
+            <i class="ri-database-2-fill" style="color: #e80368;"></i>
+              <h3><a href="">Data Repository</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
+            <i class="ri-mail-unread-fill" style="color: #e361ff;"></i>
+              <h3><a href="">Email Notification</a></h3>
+            </div>
+          </div>
+
+
+
+      </div>
+    </section><!-- End Features Section -->
+
+  
+
+
+   
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+      <div class="container">
+
+        <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/landing_assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+                <h3>Welcome to your Maritime Companion!</h3>
+                
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                 Ahoy there! Welcome aboard. This is your trusted maritime companion.
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div><!-- End testimonial item -->
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/landing_assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+                <h3>Navigate Seamlessly with this App!</h3>
+             
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Smooth sailing starts here! We provide guiding tools for hassle-free maritime journeys.
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div><!-- End testimonial item -->
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/landing_assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
+                <h3>Dive into a Sea of Features!</h3>
+         
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Dive into a sea of features! from voyage, planning, to maritime news.
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div><!-- End testimonial item -->
+
+
+  
+
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+
+
+    
+
+    <!-- ======= F.A.Q Section ======= -->
+    <section id="faq" class="faq section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>F.A.Q</h2>
+          <p>Frequently Asked Questions</p>
+        </div>
+
+        <div class="faq-list">
+          <ul>
+            <li data-aos="fade-up">
+              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1">How to download the app? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+                <p>
+                  Please contact CHED Focal for the apk installer of OBTIMCS.
+                </p>
+              </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos-delay="100">
+              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-2" class="collapsed">How to upload trainee attachments on the App? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-2" class="collapse" data-bs-parent=".faq-list">
+                <p>
+                  Go to upload evidence section, then choose file, locate the file to upload, lastly click upload button.</a>
+                </p>
+              </div>
+            </li>
+
+
+
+          </ul>
+        </div>
+
+      </div>
+    </section><!-- End F.A.Q Section -->
+
+    
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>OBTIMCS</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+
+        Powered by <a href="https://chedro9.ph/" target="_blank">CHED Region IX</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/landing_assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/landing_assets/vendor/aos/aos.js"></script>
+  <script src="assets/landing_assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/landing_assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/landing_assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/landing_assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/landing_assets/js/main.js"></script>
+
+</body>
+
+</html>
